8a3a395bc402450e018b319dd2ffd160ca96abd1
rishav-eulb <rishavkumarjee69@gmail.com>
2024-11-16 20:25:41 +0530

    Create chainConfig.ts

---
+++ frontend /scripts/chainConfig.ts
@@ -0,0 +1,40 @@
+export const chainsConfig = {
+    Arbitrum: {
+        rpc: "https://arb-sepolia.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"
+        }
+    },
+    Unichain: {
+        rpc: "https://unichain-sepolia.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x31d0220469e10c4E71834a79b1f276d740d3768F"
+        }   
+    },
+    Optimism: {
+        rpc: "https://opt-sepolia.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x5fd84259d66Cd46123540766Be93DFE6D43130D7"
+        }   
+    },
+    Polygon: {
+        rpc: "https://polygon-amoy.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582"
+        }
+    },
+    Base: {
+        rpc: "https://base-sepolia.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
+        }
+    },
+    Sepolia: {
+        rpc: "https://eth-sepolia.g.alchemy.com/v2/8EbEVKiQ3VNG1kZdvAJXG73ylk50WaSB",
+        assets: {
+            USDC: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+        }   
+    }
+};
+
+
